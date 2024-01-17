f4ab341899fce03db32e51579c6dbf9f711f560f
jst <hawciu@gmail.com>
2024-01-17 02:30:13 +0100

    finished cutscene

--- Big-Bumper-Bash/Assets/easter egg construction/easterEggConstructionScript.cs
+++ Big-Bumper-Bash/Assets/easter egg construction/easterEggConstructionScript.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class easterEggConstructionScript : MonoBehaviour
 {
     public GameObject craneEnd, easterEggObject, crane_ODD;
+    public AudioClip song;
+    AudioSource asrc;
+
+
+    public bool doonce = false;
     // Start is called before the first frame update
     void Start()
     {
-        Activate();
+        easterEggObject.SetActive(false);
+        asrc = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (doonce)
+        {
+            doonce = false;
+            Activate();
+        }
     }
 
     public void Activate()
     {
+        asrc.PlayOneShot(song, 0.1f);
         craneEnd.SetActive(false);
         crane_ODD.SetActive(false);
         easterEggObject.SetActive(true);
